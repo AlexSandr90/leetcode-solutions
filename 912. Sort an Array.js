6f4a28013f556d0365e4778fae432bb64b6ba192
Alex Gaytsuk <gayrsuk@gmail.com>
2021-11-14 19:37:05 +0200

    leetcode task 912

---
+++ 912. Sort an Array.js
@@ -0,0 +1,58 @@
+// Given an array of integers nums, sort the array in ascending order.
+
+const sortArray = nums => nums.sort((a, b) => a - b);
+
+console.log(sortArray([5, 2, 3, 1]));
+
+// const bubbleSortArray = nums => {
+//
+//     for (let i = 0; i < nums.length - 1; i++) {
+//         for (let j = 0; nums.length - 1 - i; j++) {
+//             if (nums[j + 1] < nums[j]) {
+//                 // [nums[j + 1], nums[j]] = [nums[j], nums[j + 1]]
+//                 let t = nums[j+1];
+//                 nums[j+1] = nums[j];
+//                 nums[j] = t;
+//             }
+//         }
+//     }
+//     console.log(nums);
+//     return nums;
+// };
+//
+//
+// // console.log(bubbleSortArray([5, 2, 3, 1]));
+//
+// function quickSort(arr) {
+//     let initialLeftPos = 0;
+//     let leftPos = 0;
+//     let initialRightPos = arr.length - 1;
+//     let rightPos = arr.length - 1;
+//     let direction = true;
+//     let pivot = arr.length - 1;
+//
+//     while ((leftPos - rightPos) < 0) {
+//         if (direction) {
+//             if (arr[pivot] <arr[leftPos]) {
+//                 [arr[pivot], arr[leftPos]] = [arr[leftPos], arr[pivot]];
+//                 pivot = leftPos;
+//                 rightPos--;
+//                 direction = !direction;
+//             } else {
+//                 leftPos++
+//             }
+//         } else {
+//             if (arr[pivot] <= arr[rightPos]) {
+//                 rightPos--
+//             } else {
+//                 [arr[pivot], arr[rightPos]] = [arr[rightPos], arr[pivot]];
+//                 leftPos++;
+//                 pivot = rightPos;
+//                 direction = !direction;
+//             }
+//         }
+//     }
+//
+// }
+//
+// console.log(quickSort([5, 2, 3, 1]));
